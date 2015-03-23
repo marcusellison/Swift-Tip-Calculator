662bb4891ed64a4e118847b91ebd5c6e4535703c
Marcus J. Ellison <marcus@wurkhappy.com>
2015-03-23 16:41:18 -0400

    Initial Commit

---
+++ tips2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips2
+//
+//  Created by Marcus J. Ellison on 3/23/15.
+//  Copyright (c) 2015 Marcus J. Ellison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
